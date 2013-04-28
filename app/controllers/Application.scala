af2519391c9ada966a134351825a1ed784300638
Andrew Ringler <ringler@gmail.com>
2013-04-28 17:42:17 -0400

    added basic auth

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,28 +5,26 @@ import play.api.mvc._
 import com.twilio.sdk.client.TwilioCapability
 import com.twilio.sdk.client.TwilioCapability.DomainException
 
-object Application extends Controller {
+object Application extends Controller with Secured {
+  val accountSid = Play.current.configuration.getString("twilio.accountsid").get
+  val authToken = Play.current.configuration.getString("twilio.authtoken").get
+  val applicationSid = Play.current.configuration.getString("twilio.appid").get
 
-  def index = Action {
-    val accountSid = Play.current.configuration.getString("twilio.accountsid").get
-    val authToken = Play.current.configuration.getString("twilio.authtoken").get
-    val applicationSid = Play.current.configuration.getString("twilio.appid").get
-    
-    val capability = new TwilioCapability(accountSid, authToken);
-    capability.allowClientOutgoing(applicationSid);
+  def index = IsAuthenticated { _ => _ =>
+      val capability = new TwilioCapability(accountSid, authToken);
+      capability.allowClientOutgoing(applicationSid);
 
-    var token = ""
-    try {
-      token = capability.generateToken();
-      Logger.info("Retrieved auth token from Twilio: " + token)
-    } catch {
-      case e: DomainException => {
-        Logger.error("Unable to generate auth token with Twilio", e)
-        throw new RuntimeException("Unable to generate auth token with Twilio")
+      var token = ""
+      try {
+        token = capability.generateToken();
+        Logger.info("Retrieved auth token from Twilio: " + token)
+      } catch {
+        case e: DomainException => {
+          Logger.error("Unable to generate auth token with Twilio", e)
+          throw new RuntimeException("Unable to generate auth token with Twilio")
+        }
       }
-    }
 
-    Ok(views.html.index(token))
+      Ok(views.html.index(token))
   }
-
 }
---
+++ app/controllers/Secured.scala
@@ -0,0 +1,27 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import org.apache.commons.codec.binary.Base64.decodeBase64
+
+trait Secured {
+  val basicAuthUsername = Play.current.configuration.getString("basic.auth.username").get
+  val basicAuthPassword = Play.current.configuration.getString("basic.auth.password").get
+
+  private def username(request: RequestHeader): Option[String] = {
+    request.headers.get("Authorization").flatMap { authorization =>
+      authorization.split(" ").drop(1).headOption.flatMap { encoded =>
+        new String(decodeBase64(encoded.getBytes)).split(":").toList match {
+          case username :: password :: Nil if (username == basicAuthPassword && password == basicAuthPassword) => Some(username)
+          case _ => None
+        }
+      }
+    }
+  }
+
+  private def onUnauthorized(request: RequestHeader) = Results.Unauthorized.withHeaders("WWW-Authenticate" -> "Basic realm=\"secure-mailboxes\"")
+
+  def IsAuthenticated(f: => String => Request[AnyContent] => Result) = Security.Authenticated(username, onUnauthorized) { user =>
+    Action(request => f(user)(request))
+  }
+}
