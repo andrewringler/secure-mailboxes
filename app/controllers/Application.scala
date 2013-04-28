4bf5f03a85a2df2797dbd84ea023947152afa470
Andrew Ringler <ringler@gmail.com>
2013-04-28 16:49:33 -0400

    working example of twilio client, can call and hangup against sample application

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,31 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import com.twilio.sdk.client.TwilioCapability
+import com.twilio.sdk.client.TwilioCapability.DomainException
 
 object Application extends Controller {
-  
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val accountSid = Play.current.configuration.getString("twilio.accountsid").get
+    val authToken = Play.current.configuration.getString("twilio.authtoken").get
+    val applicationSid = Play.current.configuration.getString("twilio.appid").get
+    
+    val capability = new TwilioCapability(accountSid, authToken);
+    capability.allowClientOutgoing(applicationSid);
+
+    var token = ""
+    try {
+      token = capability.generateToken();
+      Logger.info("Retrieved auth token from Twilio: " + token)
+    } catch {
+      case e: DomainException => {
+        Logger.error("Unable to generate auth token with Twilio", e)
+        throw new RuntimeException("Unable to generate auth token with Twilio")
+      }
+    }
+
+    Ok(views.html.index(token))
   }
-  
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,16 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "secure-mailboxes"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "secure-mailboxes"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     jdbc,
-    anorm
-  )
+    anorm,
+    "com.twilio.sdk" % "twilio-java-sdk" % "3.3.15")
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+  val main = play.Project(appName, appVersion, appDependencies).settings( // Add your own project settings here      
   )
 
 }
